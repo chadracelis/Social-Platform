459c3af248b443f3e6b7823ac0b8e4c845876d1d
Chad <49378047+Cchadwwick@users.noreply.github.com>
2019-07-03 22:28:26 -0400

    Update authActions.js
    delete unnecessary comments

--- basic_social_platform/src/store/actions/authActions.js
+++ basic_social_platform/src/store/actions/authActions.js
@@ -1,35 +1,3 @@
-/*
-const signIn = (credentials) => {
-    return (dispatch, getState, {getFirebase}) => {
-        const firebase = getFirebase();
-
-        firebase.auth().signInWithEmailAndPassword(
-            credentials.email,
-            credentials.password
-        ).then(() => {
-            dispatch({ type: 'LOGIN_SUCCESS'});
-        }).catch((err) => {
-            dispatch({ type: 'LOGIN_ERROR', err});
-        })
-    }
-}
-
-
-const signOut = () => {
-    return (dispatch, getState, {getFirebase}) => {
-        const firebase = getFirebase();
-
-        firebase.auth().signOut().then(() => {
-            dispatch({ type: 'SIGNOUT_SUCCESS' });
-        })
-    }
-}
-
-
-export default (signIn, signOut)
-
-*/
-
 export const signIn = (credentials) => {
     return (dispatch, getState, {getFirebase}) => {
       const firebase = getFirebase();
