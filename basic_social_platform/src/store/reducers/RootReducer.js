446ef35a91bf0a4dc00da69408ba0986520fa8e2
Chad <49378047+Cchadwwick@users.noreply.github.com>
2019-07-03 22:29:10 -0400

    Update RootReducer.js

--- basic_social_platform/src/store/reducers/RootReducer.js
+++ basic_social_platform/src/store/reducers/RootReducer.js
@@ -1,7 +1,7 @@
 import AuthReducer from './AuthReducer'
 import ProjectReducer from './ProjectReducer'
 import { combineReducers } from 'redux'
-import { firestoreReducer } from 'redux-firestore' // syncs firestore with the redux state
+import { firestoreReducer } from 'redux-firestore' 
 import { firebaseReducer } from 'react-redux-firebase'
 
 const RootReducer = combineReducers({
