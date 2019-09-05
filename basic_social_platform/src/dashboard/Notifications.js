9a54640e6d9c8ac5651b3ba42e5cb1bf1848c9b0
Chad <49378047+chadracelis@users.noreply.github.com>
2019-09-05 19:54:21 -0400

    update classes

--- basic_social_platform/src/dashboard/Notifications.js
+++ basic_social_platform/src/dashboard/Notifications.js
@@ -6,16 +6,19 @@ const Notifications = (props) => {
   return (
     <div className="section">
       <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Notifications</span>
+        <div className="card-content notifications-card">
+          <h5>Notifications</h5>
           <ul className="online-users">
             { notifications && notifications.map(notification =>{
-              return <li key={notification.id}>
-                <span className="light-green-text">{notification.user} </span>
-                <span>{notification.content}</span>
-                <div className="note-date grey-text">{moment(notification.time.toDate()).fromNow()}</div>
-              </li>
-            })}
+              return (
+                <li key={notification.id}>
+                  <span className="light-green-text"> {notification.user} </span>
+                  <span> {notification.content} </span>
+                  <div className="note-date grey-text">
+                    {moment(notification.time.toDate()).fromNow()}
+                  </div>
+                </li>
+              )})}
           </ul>
         </div>
       </div>
